fd2f2a3240075f410da0742dab24e3107d805872
Michael I <miodice3@gmail.com>
2020-09-21 19:15:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -77,10 +77,10 @@ def draw?(board)
     position_2 = board[win_index_2] # load the value of the board at win_index_2
     position_3 = board[win_index_3] # load the value of the board at win_index_3    
 
-    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-      puts "false C"
-     return false
-    end
+        if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+          puts "false C"
+         return false
+        end
   end
 end
 
