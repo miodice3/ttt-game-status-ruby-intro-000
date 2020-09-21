bc3c636a4a26fd9f278efb6db2dd1ea0f32fecb1
Michael I <miodice3@gmail.com>
2020-09-21 19:31:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -93,7 +93,21 @@ board = ["X", "X", "O", "X", "X", "X", "X", "X", "X"]   #win
 
 def over?(board)
   #accepts BOARD, TRUE if board WON, TRUE if board DRAW, TRUE if board full
-  puts won?(board)
+            WIN_COMBINATIONS.each do |win_combination|
+          #puts win_combination.inspect
+          win_index_1 = win_combination[0]
+          win_index_2 = win_combination[1]
+          win_index_3 = win_combination[2]
+          
+          position_1 = board[win_index_1] # load the value of the board at win_index_1
+          position_2 = board[win_index_2] # load the value of the board at win_index_2
+          position_3 = board[win_index_3] # load the value of the board at win_index_3    
+    
+            if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
+             puts "true A"
+             
+             true
+            end
 end
 
 over?(board)
