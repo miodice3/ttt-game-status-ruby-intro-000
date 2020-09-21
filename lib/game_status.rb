4b8a9207809e0e2bab8480fb9adf709a19660e6e
Michael I <miodice3@gmail.com>
2020-09-21 19:50:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -123,7 +123,6 @@ end
 #board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]   #draw
 #board = ["X", "X", "O", "X", "X", "X", "X", "X", "X"]   #win
 
-=begin
 def winner(board)
+#accepts BOARD, return "X" or "O" that has won the game. return FALSE if no winner
 end
-=end
