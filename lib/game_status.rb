601288dc8eaf69a52e635516ce2694f1be7e8fbb
Michael I <miodice3@gmail.com>
2020-09-21 19:27:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -93,7 +93,7 @@ board = ["X", "X", "O", "X", "X", "X", "X", "X", "X"]   #draw
 
 def over?(board)
   #accepts BOARD, TRUE if board WON, TRUE if board DRAW, TRUE if board full
-  if won?(board)
+  puts won?(board)
 end
 
 =begin
